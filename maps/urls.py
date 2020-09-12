e11aa4ad2c335905c9b518372de3d8e54f12e0e8
Jonathan Mo <jm9hx@virginia.edu>
2020-09-12 14:34:19 -0400

    moved gitignore, requirements.txt, README.md

---
+++ maps/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'maps'
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    # path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    # path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    # path('<int:question_id>/vote/', views.vote, name='vote'),
+    # path('comments/', views.comments, name='comments'),
+    # path('comments/list', views.comments_list, name='comments_list'),
+
+]
