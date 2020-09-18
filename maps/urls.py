551ec324bf1b2cfd18cd3a128b1fda66fdd9953d
ncooney <npc4crc@virginia.edu>
2020-09-18 18:12:19 -0400

    integrated Google login API

--- maps/urls.py
+++ maps/urls.py
@@ -1,10 +1,14 @@
-from django.urls import path
+from django.urls import path, include
+from django.contrib import admin
+from django.views.generic import TemplateView
+from django.conf.urls import url
 
 from . import views
 
 app_name = 'maps'
 urlpatterns = [
     path('', views.Index.as_view(), name='index'),
+    url('^soc/', include('social_django.urls', namespace='social'))
     # path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     # path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     # path('<int:question_id>/vote/', views.vote, name='vote'),
