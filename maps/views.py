edb8a5621fa26a9f3ae4b83f7a847196bf6ab000
Jonathan Mo <jm9hx@virginia.edu>
2020-09-25 10:34:36 -0400

    Added logout ability

--- maps/views.py
+++ maps/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 from django.views.generic.list import ListView
+from django.contrib.auth import logout
 
 # Create your views here.
 class Index(ListView):
@@ -8,3 +9,6 @@ class Index(ListView):
 
     def get_queryset(self):
         pass
+
+def logout(request):
+    logout(request)
