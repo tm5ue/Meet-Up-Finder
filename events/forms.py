37f97af26fccd8ff53e896adb8d61ef879bc683a
Jonathan Mo <jm9hx@virginia.edu>
2020-09-27 12:52:24 -0400

    events v0.1, minimal functionality

---
+++ events/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from django.forms import ModelForm
+from .models import Event
+
+class EventForm(ModelForm):
+
+    class Meta:
+        model = Event
+        fields = ['name', 'description']
