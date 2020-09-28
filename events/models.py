e1a3c4e4616c97dff8989b7fca131c253a94fb43
Jonathan Mo <jm9hx@virginia.edu>
2020-09-27 22:38:23 -0400

    Added events v0.2, next iteration work on date input field, location input field

--- events/models.py
+++ events/models.py
@@ -1,12 +1,22 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
 
 class Event(models.Model):
     name = models.CharField(max_length=200, null=True)
     description = models.TextField(null=True)
-    # pub_date = models.DateTimeField('date published')
-    comments = []
+    pub_date = models.DateTimeField()
+    event_date = models.DateTimeField(null=False, default=timezone.localtime())
+    # start_date = models.DateTimeField(null=False, default=timezone.localtime())
+    # end_date = models.DateTimeField(null=False, default=timezone.localtime())
+    author = models.CharField(max_length=200, null=False, default="no author")
+    # TODO: fix date input field (find interactive calendar to input date and time)
+    # TODO: location (figure out how to integrate maps of some sort similar to https://github.com/caioariede/django-location-field
+    # TODO: comments,
+    # TODO: tags (sports, music, etc)
+    #location =
+    #comments = []
+
 
     def __str__(self):
         return self.name.title()
--- events/urls.py
+++ events/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 from django.conf.urls import url
 from django.conf import settings
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import LogoutView, LoginView
 
 from . import views
 
@@ -12,8 +12,9 @@ app_name = 'events'
 urlpatterns = [
     path('', views.Index.as_view(), name='index'),
     url('^soc/', include('social_django.urls', namespace='social')),
-    path('login/', LogoutView.as_view(template_name="events/index.html"), name='logout'),
-
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout', LogoutView.as_view(template_name='events/index.html'), name='logout'),
     path('events/add', views.AddEvent.as_view(), name='add_event'),
+    path('events/<int:event_id>/', views.Detail.as_view(), name='detail'),
 
 ]
