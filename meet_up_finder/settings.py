0d24dad279124808cf38e7c88193b0bdab0d7d81
Jonathan Mo <jm9hx@virginia.edu>
2020-10-02 16:06:10 -0400

    Added unit tests for event application v3

--- meet_up_finder/settings.py
+++ meet_up_finder/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+if "CI" in os.environ:
+    config['TEST_RUNNER'] = 'django_heroku.HerokuDiscoverRunner'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
