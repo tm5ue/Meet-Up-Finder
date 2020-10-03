a23f063ac87314326bac2a2cf03312352a8b7f3e
Jonathan Mo <jm9hx@virginia.edu>
2020-10-03 10:59:00 -0400

    Added unit tests for event application v1.20

--- meet_up_finder/settings.py
+++ meet_up_finder/settings.py
@@ -86,6 +86,9 @@ DATABASES = {
         'PASSWORD': 'meetup-finder',
         'HOST': '127.0.0.1',
         'PORT': '5432',
+        'TEST': {
+            'NAME': 'mytestdatabase',
+        }
     }
 }
 
