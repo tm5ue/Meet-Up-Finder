25fa48c1b0a0add35ac93733f5372c130a36a662
Simran Kaur <sk9za@virginia.edu>
2020-10-11 20:15:45 -0400

    Change Site ID
    Changing the site ID to 2. Perhaps will fix?

--- meet_up_finder/settings.py
+++ meet_up_finder/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'meet_up_finder.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'meetup_finder',
+        'NAME': 'meetup-finder',
         'USER': 'nickc',
         'PASSWORD': 'meetup-finder',
         'HOST': '127.0.0.1',
@@ -140,13 +140,13 @@ AUTHENTICATION_BACKENDS = (
 
 SOCIAL_AUTH_URL_NAMESPACE = 'social'
 
-try:
-    SITE_ID = 1
-except:
-    SITE_ID = 2
-else:
-    SITE_ID = 3
-#SITE_ID=3
+#try:
+#    SITE_ID = 1
+#except:
+#    SITE_ID = 2
+#else:
+#    SITE_ID = 3
+SITE_ID=2
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
