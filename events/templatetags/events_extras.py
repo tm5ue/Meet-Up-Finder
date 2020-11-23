907c9cc2926a9b704d040e4cb22e28bd27e723a3
Nick Cooney <70594407+ncooney@users.noreply.github.com>
2020-11-23 00:54:41 -0500

    Update events_extras.py

--- events/templatetags/events_extras.py
+++ events/templatetags/events_extras.py
@@ -5,14 +5,10 @@ register = template.Library()
 @register.filter(name='is_marked')
 def is_marked(value, arg):
     """Checks if user is marked, for use in attendees and bookmarkers"""
-    
     already_marked = False
-    # line=""
 
     for user in value.all() :
-        # line = line + str(user_bookmarked)
         if (arg == user) :
             already_marked = True
 
     return already_marked
-
